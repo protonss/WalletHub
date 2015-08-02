56a3c4cb3a2576642adc6404e87c2796a5f28ab6
Anselmo Ribeiro <6975.usa@gmail.com>
2015-08-02 18:19:16 -0300

    create second option for question 01

--- WalletHub/mysql/MysqlQuestion01.sql
+++ WalletHub/mysql/MysqlQuestion01.sql
@@ -1,6 +1,12 @@
-select name, sum(votes) as votes,  @rn := @rn -1 as rank
-from votes cross join
-     (select @rn := (select count(*) from votes)) variable
-group by name, votes
-order by votes desc;
+SELECT name, votes, @rank := @rank + 1 AS rank
+FROM  votes v, (SELECT @rank := 0) r
+ORDER BY  votes desc;
 
+SELECT name, votes,
+CASE 
+WHEN @pRank = votes THEN @rank 
+WHEN @pRank := votes THEN @rank := @rank + 1
+END AS rank
+FROM votes v,
+(SELECT @rank :=0, @pRank := NULL) r
+ORDER BY votes desc
