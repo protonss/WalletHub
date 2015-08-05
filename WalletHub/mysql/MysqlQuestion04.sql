076f426418491e35b7ff064fdcec92fccb69aee6
Anselmo S Ribeiro <6975.usa@gmail.com>
2015-08-05 10:56:44 -0300

    mysql question #4 update

--- WalletHub/mysql/MysqlQuestion04.sql
+++ WalletHub/mysql/MysqlQuestion04.sql
@@ -2,7 +2,7 @@
 set @start_date := '2011-06-20';
 set @end_date := '2011-06-22';
 
-select * 
+select count(*) as 'Number of bugs open' 
 from bugs
 where (open_date  <= STR_TO_DATE(@start_date, '%Y-%m-%d') and close_date > STR_TO_DATE(@start_date, '%Y-%m-%d'))
    and (open_date  <= STR_TO_DATE(@end_date, '%Y-%m-%d') and close_date > STR_TO_DATE(@end_date, '%Y-%m-%d')) ;
